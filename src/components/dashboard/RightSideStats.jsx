e830be984085dffbd5c86f9f7d160c16d35abeaa
Ali136384 <haseniali920@gmail.com>
2024-11-17 14:12:12 +0300

    edits

--- src/components/dashboard/RightSideStats.jsx
+++ src/components/dashboard/RightSideStats.jsx
@@ -1,10 +1,8 @@
 import React, { useEffect, useState } from "react";
 import CountUp from "react-countup";
-import {
-  getAllCustomers,
-  getTotalIncome,
-  getTotalSalaries,
-} from "../../app/api/v1/customer";
+import { getTotalSalaries } from "../../app/api/v1/user";
+
+import { getAllCustomers, getTotalIncome } from "../../app/api/v1/customer";
 import { months } from "./data";
 const MoneyStats = () => {
   const [selectedOptionMoney, setSelectedOptionMoney] = useState("TL");
