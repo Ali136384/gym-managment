ff919aeb655c87f573484d1562073138a1902d80
Ali136384 <haseniali920@gmail.com>
2024-11-21 14:17:01 +0300

    typeof window

--- src/app/panel/page.jsx
+++ src/app/panel/page.jsx
@@ -1,4 +1,5 @@
 "use client";
+
 import React, { useEffect, useState } from "react";
 import Chart from "../../components/dashboard/Chart";
 import Stats_top from "../../components/dashboard/StatsTop";
