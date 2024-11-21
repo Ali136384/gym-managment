646417092ac1ad68d852603a2118ed811c225f1b
Ali136384 <haseniali920@gmail.com>
2024-11-21 14:32:40 +0300

    typeof window

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  i18n: {
-    locales: ["ar", "en", "fr", "nl-NL"],
-    defaultLocale: "en",
-  },
-  // Enables static export
   output: "export",
 };
 
