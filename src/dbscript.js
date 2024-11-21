faf44d13a75b2330fbc66a7ca70b6b373cb5e028
Henry Markle <mohammad96alsalloum@outlook.sa>
2024-11-21 18:24:33 +0300

    Prisma file

--- src/dbscript.js
+++ src/dbscript.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 
-const sourcePath = path.resolve('prisma/dev.db');  // Path to the SQLite file in the project
+const sourcePath = path.resolve('dev.db');  // Path to the SQLite file in the project
 const targetPath = '/tmp/dev.db';  // Path where SQLite will be stored in Vercel's writable space
 
 // Copy the database to /tmp if it doesn't already exist there
